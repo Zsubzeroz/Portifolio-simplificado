82cd97e75a3dc1ea0acbe9092cf627c095e4cc3d
LUAN ESTIFER RODRIGUES PEREIRA <113989498+Zsubzeroz@users.noreply.github.com>
2025-07-02 19:38:14 -0300

    Delete Curriculo directory

--- Curriculo/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('download-pdf').addEventListener('click', function() {
-    window.print();
-});
-
-document.getElementById('imprimir').addEventListener('click', function() {
-    window.print();
-});
